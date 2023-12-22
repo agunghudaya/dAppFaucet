cb971abe01b0e109a81e46cfb8c57a9d2a0bb426
Agung Kurnia Hudaya <agung.hudaya@tokopedia.com>
2023-12-23 00:33:25 +0700

    home design done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <>
+      <div className='faucet-wrapper'>
+        <div className='faucet'>
+          <div className='balance-view is-size-2'>
+            Current Balance: <strong>10</strong>
+          </div>
+          <button className='btn mr-2'>Donate</button>
+          <button className='btn mr-2'>Withdraw</button>
+        </div>
+      </div>
+    </>
   );
 }
 
