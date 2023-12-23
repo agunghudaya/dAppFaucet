18ba5b9d2c91a6347b029dcdda8e90b6a7c1e8f8
Agung Kurnia Hudaya <agung.hudaya@tokopedia.com>
2023-12-23 10:23:28 +0700

    integrated to metamask

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+
+  useEffect(() => {
+    const loadProvider = async () => {
+
+    }
+     loadProvider()
+  }, [])
+
+
   return (
     <>
       <div className='faucet-wrapper'>
@@ -9,6 +19,16 @@ function App() {
           <div className='balance-view is-size-2'>
             Current Balance: <strong>10</strong>
           </div>
+          <button
+            className="btn mr-2"
+            onClick={async () => {
+              const account = await window.ethereum.request({method: "eth_requestAccounts"})
+              console.log(account)
+            }}
+          >
+            Enable ETH
+          </button>
+
           <button className='btn mr-2'>Donate</button>
           <button className='btn mr-2'>Withdraw</button>
         </div>
